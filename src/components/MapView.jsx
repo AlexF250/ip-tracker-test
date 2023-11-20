40f23229912533d77af289e070fd1e31dab4f4a0
AlexF250 <gotchaf250@gmail.com>
2023-11-20 12:03:29 -0800

    marker files

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -7,6 +7,17 @@ export function MapView () {
 
     const {data} = useContext(MapContext);
 
+    var greenIcon = L.icon({
+        iconUrl: '../../public/img/distmarker-icon.png',
+        shadowUrl: '../../public/img/distmarker-shadow.png',
+    
+        iconSize:     [38, 75], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
     function SetPosition () {
         const map = useMap();
         useEffect(() => {
@@ -17,7 +28,7 @@ export function MapView () {
 
     return <MapContainer center={{lat: data.location.lat, lng: data.location.lng}} zoom={13} className='z-10'>
         <TileLayer url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'></TileLayer>
-        <Marker position={{lat: data.location.lat, lng: data.location.lng}} />
+        <Marker position={{lat: data.location.lat, lng: data.location.lng}} icon={greenIcon}/>
         <SetPosition/>
     </MapContainer>
 }
