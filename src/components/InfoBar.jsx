405b639c2edc9843a75a6f9fffed96168dc490fe
AlexF250 <117200586+AlexF250@users.noreply.github.com>
2023-11-20 15:07:01 -0600

    Update InfoBar.jsx

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -10,7 +10,7 @@ const {data} = useContext(MapContext);
 
 
   return (
-              <div className="grid p-6 bg-slate-50 w-9/12 mx-auto mt-32 rounded-xl z-40 relative md:text-justify text-center md:grid-cols-4">
+              <div className="grid p-6 bg-slate-50 w-9/12 mx-auto mt-[3%] rounded-xl z-40 relative md:text-justify text-center md:grid-cols-4">
             <div className="border-e-slate-200 md:border-e-2 border-e-0 md:ps-0 ps-4">
               <h6 className="text-xs uppercase text-slate-400 font-bold">ip address</h6>
               <p className="text-2xl">{data.ip}</p>
